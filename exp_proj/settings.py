0f5b182495c0760a23455e0d22f33087cd01455c
Dharmik564 <dharmikmodi564@gmail.com>
2025-04-29 15:55:21 +0530

    Add Middleware for whitenoise in settings.py file

--- exp_proj/settings.py
+++ exp_proj/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # add this line after SecurityMiddleware
 ]
 
 ROOT_URLCONF = 'exp_proj.urls'
